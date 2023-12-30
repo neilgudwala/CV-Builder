9ec6d7a1bddcab88c5e3e58fcd95b0ba17eec454
Neil <neil.gudwala@gmail.com>
2023-12-30 21:24:50 +0530

    Personal Info and About section built

---
+++ src/components/Summary.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import '../styles/Summary.css'
+
+function Summary() {
+  const [summary, setSummary] = React.useState('Experienced web developer with a passion for creating user-friendly and visually appealing websites. Skilled in HTML, CSS, and JavaScript, with a focus on front-end development. Proven track record of delivering high-quality projects on time and within budget.');
+  return (
+    <div>
+      <h1 className='summary'>About Me</h1>
+      <div className='line'></div>
+      <textarea className='summary-box' name="summary" value={summary} onChange={(e) => setSummary(e.target.value)} placeholder='What makes you stand out?' cols="30" rows="5"></textarea>
+    </div>
+  );
+}
+
+
+export {Summary}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {App} from './App.jsx'
+import {Summary} from './components/Summary.jsx'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='display-modal'>
+    <App className='input-block' />
+    <Summary/>
+    </div>
+    
+    
   </React.StrictMode>,
 )
